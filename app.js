6d2e1c4609706b88c98b7dfbba5ba9f819c5456a
higor <higorvitorpadilha@gmail.com>
2024-09-10 22:49:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+var classname = document.body.className;
+if(classname== "light-theme") {
+    this.textContent = "dark";
+}
+else {
+    this.textContent = "light";
+}
+console.log('current class name: ' + className);
+});
